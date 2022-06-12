8acfc65312f1588e4cc8d12bf6f70cd4a663ac01
mainzonder <no7au53r@gmail.com>
2022-06-12 15:17:39 +0300

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
-import styled from 'styled-components';
-import {useSelector,useDispatch} from 'react-redux';
-import {useEffect} from 'react';
-import {Link} from 'react-dom';
-import {IoMoon, IoMoonOutline} from 'react-icons/io5';
+import styled from "styled-components";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { Link } from "react-dom";
+import { IoMoon, IoMoonOutline } from "react-icons/io5";
 
-import {Container} from './Container'
-import{setTheme} from '../store/theme/theme-actions'
+import { Container } from "./Container";
+import { setTheme } from "../store/theme/theme-actions";
 
+const HeaderEl = styled.header`
+    box-shadow: var(--shadow);
+    background-color: var(--colors-ui-base);
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 0;
+`;
